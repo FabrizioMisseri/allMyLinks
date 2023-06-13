21688e4d153e87ff7e54c609daadfdabd837852b
FabrizioMisseri <misserifabrizio@gmail.com>
2023-06-13 11:12:39 +0200

    add links

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script>
+import { store } from '../store';
+
+export default {
+  name: "App",
+
+  data() {
+    return {
+      store,
+    }
+  }
+}
 </script>
 
 <template>
   <section class="wrapper p-5">
     <div class="container-fluid my-5 py-3 fs-5 mx-4 px-4">
 
-      <div class="card record px-4 py-3">
-        <a href="#">
-          sakgfjaslfgksdgjlsgjsdhjfsd
+      <div class="btn card record px-4 py-3 my-4" v-for="(item, index) in store.array" :key="index">
+        <a :href="item.link">
+          {{ item.name }}
         </a>
       </div>
 
---
+++ store.js
@@ -0,0 +1,28 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+
+    array: [
+        {
+            name: "Fabrizio Misseri",
+            link: "https://legendary-capybara-9207a4.netlify.app/",
+        },
+        {
+            name: "sito a cazzo",
+            link: "https://sitoacazzo.netlify.app/",
+        },
+        {
+            name: "chatgpt-italiano(IP-catcher)",
+            link: "https://iplogger.com/22rhs5",
+        },
+        {
+            name: "esplodi PC-windows",
+            link: "https://esplodi-pc.netlify.app/",
+        },
+        {
+            name: "Federica Misseri",
+            link: "#",
+        },
+    ],
+
+});
