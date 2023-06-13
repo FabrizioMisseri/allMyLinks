319eec32c0ec6a04f3888a3fcb2d1d9299cfa259
FabrizioMisseri <misserifabrizio@gmail.com>
2023-06-13 10:50:55 +0200

    add general-style

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,17 @@
 </script>
 
 <template>
-  <h1>ciao sono link</h1>
+  <section class="wrapper p-5">
+    <div class="container-fluid my-5 py-3 fs-5 mx-4 px-4">
+
+      <div class="card record px-4 py-3">
+        <a href="#">
+          sakgfjaslfgksdgjlsgjsdhjfsd
+        </a>
+      </div>
+
+    </div>
+  </section>
 </template>
 
 <style></style>
