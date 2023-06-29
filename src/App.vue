49ce19f2b0b1ac859cb79cb4a6991a51e7df0925
FabrizioMisseri <misserifabrizio@gmail.com>
2023-06-29 09:17:13 +0200

    *

--- src/App.vue
+++ src/App.vue
@@ -19,11 +19,11 @@ export default {
 
     <div class="container-fluid">
 
-      <div class="btn card record px-4 py-3 my-4" v-for="(item, index) in store.array" :key="index">
-        <a class="fs-5" :href="item.link">
-          {{ item.name }}
-        </a>
-      </div>
+      <a class="btn card record px-4 py-3 my-4 fs-5" v-for="(item, index) in store.array" :href="item.link" :key="index">
+
+        {{ item.name }}
+
+      </a>
 
       <!-- <div class="btn card record px-4 py-3 my-4" v-for="(item, index) in store.array" :key="index">
         <a class="fs-5" :href="item.link">
