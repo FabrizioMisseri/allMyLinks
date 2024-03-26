789addcf6cc7fe9c1ae75d589021c9b7f6c87296
FabrizioMisseri <misserifabrizio@gmail.com>
2024-03-26 20:15:55 +0100

    *

--- store.js
+++ store.js
@@ -3,42 +3,63 @@ import { reactive } from "vue";
 export const store = reactive({
 
     array: [
+        // {
+        //     name: "Fabrizio Misseri",
+        //     link: "https://fabrizio-misseri.netlify.app/",
+        // },
+        // {
+        //     name: "Cristian Badei",
+        //     link: "https://cristian-steel-body.netlify.app/",
+        // },
+        // {
+        //     name: "Rigenera il Seed",
+        //     link: "https://recover-seed-tool.netlify.app/",
+        // },
+        // {
+        //     name: "chatgpt-italiano (leak dati utente inconsapevole)",
+        //     link: "https://iplogger.com/22rhs5",
+        // },
+        // {
+        //     name: "esplodi PC-windows",
+        //     link: "https://esplodi-pc.netlify.app/",
+        // },
+        // {
+        //     name: "diploma Boolean",
+        //     link: "https://www.credential.net/d8ef9689-a45c-4e52-9597-920f5b8fbcd5#gs.1tz5b0"
+        // },
+        // {
+        //     name: "diploma Experis",
+        //     link: "https://certificazione-java-fabrizio-misseri.netlify.app"
+        // },
+        // {
+        //     name: "GitHub",
+        //     link: "https://github.com/FabrizioMisseri?tab=repositories",
+        // },
+        // {
+        //     name: "Federica Misseri",
+        //     link: "https://federica-misseri.netlify.app/",
+        // },
         {
-            name: "Fabrizio Misseri",
-            link: "https://fabrizio-misseri.netlify.app/",
+            name: "Bella",
+            link: "https://www.bakecaincontrii.com/annuncio/novita-solo-da-te-strabellissima-bambola-ardente-sensuale-bravissima-5tadi-tette-preliminari-da-urlo-it7j8evkc/?pck=abdb84",
         },
         {
-            name: "Cristian Badei",
-            link: "https://cristian-steel-body.netlify.app/",
+            name: "Luna",
+            link: "https://www.bakecaincontrii.com/annuncio/latobomba-sexy-foto-vere-pronta-a-farti-impazzire-preliminari-da-infarto-it2qvdr8r/",
         },
         {
-            name: "Rigenera il Seed",
-            link: "https://recover-seed-tool.netlify.app/",
+            name: "Alessia",
+            link: "https://www.bakecaincontrii.com/annuncio/alessia-raggiungoin-tutta-la-citta-appena-arrivata-foto-100-originali-it68a2e9d/",
         },
         {
-            name: "chatgpt-italiano (leak dati utente inconsapevole)",
-            link: "https://iplogger.com/22rhs5",
+            name: "2 amichette",
+            link: "https://www.bakecaincontrii.com/annuncio/affascinante-sexy-dolcecurve-fantastiche-giovane-23-anni-figa-da-impazzireti-raggiungo-volendo-con-a-it53iw4iy/?pck=bbb7e2",
         },
         {
-            name: "esplodi PC-windows",
-            link: "https://esplodi-pc.netlify.app/",
+            name: "Sofia",
+            link: "https://www.bakecaincontrii.com/annuncio/novita-assoluta-sofia-bocca-caldissima-gola-profonda-sexy-una-figa-calda-e-bagnata-it2utd6w2/",
         },
-        {
-            name: "diploma Boolean",
-            link: "https://www.credential.net/d8ef9689-a45c-4e52-9597-920f5b8fbcd5#gs.1tz5b0"
-        },
-        {
-            name: "diploma Experis",
-            link: "https://certificazione-java-fabrizio-misseri.netlify.app"
-        },
-        {
-            name: "GitHub",
-            link: "https://github.com/FabrizioMisseri?tab=repositories",
-        },
-        {
-            name: "Federica Misseri",
-            link: "https://federica-misseri.netlify.app/",
-        },
+
     ],
 
 });
